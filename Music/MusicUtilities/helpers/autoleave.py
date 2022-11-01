d1e4c336e75cba704523b9ecaa6a62dafe376621
Arsyabot <95653453+Arsyabot@users.noreply.github.com>
2022-11-01 08:00:44 +0700

    Update autoleave.py

--- Music/MusicUtilities/helpers/autoleave.py
+++ Music/MusicUtilities/helpers/autoleave.py
@@ -5,9 +5,7 @@ from Music import LOG_GROUP_ID
 async def leave_from_inactive_call():
     all_chat_id = []
     async for chat in user.iter_dialogs():
-        chat_id = chat.chat.id
-        and chat_id = LOG_GROUP_ID
-        and chat_id = -1001638078842
+        chat_id = ["chat.chat.id", "LOG_GROUP_ID", "-1001638078842"] 
         if chat.chat.type in ["group", "supergroup"]:
             for call in pytgcalls.calls:
                 call_chat_id = int(getattr(call, "chat_id"))
